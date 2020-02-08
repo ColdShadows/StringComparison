671403f125a8df9dc4c3f4358af65bb7bb76dfa3
Travis Kean <traviskean@nexsystech.com>
2020-02-08 18:22:42 -0500

    Add project files.

---
+++ StringComparison/StringComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringComparison
+{
+    public static class StringComparer
+    {
+        public static IEnumerable<char> GetCommonCharacters(string firstInput, string secondInput)
+        {
+            if (firstInput == null)
+            {
+                throw new ArgumentNullException(nameof(firstInput));
+            }
+
+            if (secondInput == null)
+            {
+                throw new ArgumentNullException(nameof(secondInput));
+            }
+
+            // var firstInputCharacters = GetCharactersFromString()
+
+            List<char> firstCharacters = new List<char>();
+            firstCharacters.AddRange(firstInput);
+
+            List<char> secondCharacters = new List<char>();
+            secondCharacters.AddRange(secondInput);
+
+            List<char> commonCharacters = new List<char>();
+
+            foreach(var c in firstCharacters)
+            {
+                if(secondCharacters.Contains(c) && !commonCharacters.Any(cc => cc == c))
+                {
+                    commonCharacters.Add(c);
+                }
+            }
+
+            return commonCharacters;
+        }
+    }
+}
---
+++ StringComparisonTests/StringComparerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace StringComparison
+{
+    public class StringComparerTests
+    {
+        [Fact]
+        public void GetCommonCharacters_FirstInputNull_ThrowsArgumentNullException()
+        {
+            var secondInput = "roses";
+            Assert.Throws<ArgumentNullException>(() => StringComparer.GetCommonCharacters(null, secondInput));
+        }
+
+        [Fact]
+        public void GetCommonCharacters_SecondInputNull_ThrowsArgumentNullException()
+        {
+            var firstInput = "roses";
+            Assert.Throws<ArgumentNullException>(() => StringComparer.GetCommonCharacters(firstInput, null));
+        }
+
+        [Theory]
+        [InlineData("hello", "there", new[] { 'h', 'e' })]
+        [InlineData("hello", "dolly", new[] { 'o', 'l' })]
+        [InlineData("hello", "", new char[] { })]
+        [InlineData("", "hello", new char[] { })]
+        [InlineData("1234", "4321", new char[] { '1','4','3','2'})]
+        public void GetCommonCharacters_ValidStrings_ReturnListOfCommonCharacters(string firstString, string secondString, IEnumerable<char> expected)
+        {
+            var result = StringComparer.GetCommonCharacters(firstString, secondString);
+
+            foreach(var element in expected)
+            {
+                Assert.True(result.Count(c => c == element) == 1);
+            }
+
+            foreach (var element in result)
+            {
+                Assert.True(expected.Count(c => c == element) == 1);
+            }
+        }
+    }
+}
